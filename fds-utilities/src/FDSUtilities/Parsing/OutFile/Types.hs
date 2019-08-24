63507c6f5f7b0dfa63f7f3821d62412db8b1277e
Jake O'Shannessy <joshannessy@gmail.com>
2019-08-24 11:24:50 -0400

    fute-core: Add aeson typeclasses for CurrentProgress.

--- fds-utilities/src/FDSUtilities/Parsing/OutFile/Types.hs
+++ fds-utilities/src/FDSUtilities/Parsing/OutFile/Types.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module FDSUtilities.Parsing.OutFile.Types where
 
+import qualified Control.Applicative as A
+import Control.Monad (mzero)
+import Data.Aeson as Aeson
+import Data.Aeson.Types
 import Data.Time
+import Data.Text
 
 data CurrentProgress = CurrentProgress
     { currentProgress_endTime :: Double
@@ -8,6 +14,23 @@ data CurrentProgress = CurrentProgress
     , currentProgress_lastWallTime :: UTCTime
     , currentProgress_currentWallTime :: UTCTime
     } deriving (Show)
+
+instance ToJSON CurrentProgress where
+    toJSON currentProgress =
+        object
+            [ "EndTime" Aeson..= currentProgress_endTime currentProgress
+            , "LastSimTime" Aeson..= currentProgress_lastSimTime currentProgress
+            , "LastWallTime" Aeson..= currentProgress_lastWallTime currentProgress
+            , "CurrentWallTime" Aeson..= currentProgress_currentWallTime currentProgress
+            ]
+
+instance FromJSON CurrentProgress where
+    parseJSON (Object v) = CurrentProgress A.<$>
+                                v .: "EndTime" A.<*>
+                                v .: "LastSimTime" A.<*>
+                                v .: "LastWallTime" A.<*>
+                                v .: "CurrentWallTime"
+    parseJSON _ = mzero
 
 data MeshGridDimensions = MeshGridDimensions
     Int -- ^MeshNum
